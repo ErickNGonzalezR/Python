003ce2e9756908896b1186fa242de9987cd03583
Erick Nicolas Gonzalez <nicolo288@hotmail.com>
2022-10-04 10:25:22 -0500

    agrege un nuevo ejercicio

---
+++ ejercicios_python/Condicionales.py
@@ -0,0 +1,13 @@
+# edad = int(input("Escibe tu edad : "))
+# if edad > 17:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de edad")
+
+numero = int(input("Escribe un numero"))
+if numero > 5:
+    print(str(numero) +" es mayor que 5")
+elif numero == 5:
+    print(str(numero) + "es igual que 5")
+else:
+    print(str(numero) + " es menor que 5 ")
---
+++ ejercicios_python/Conversor_Monedas.py
@@ -0,0 +1,7 @@
+pesos = float(input ("Cuantos pesos Colombianos tienes"))
+valor_dolar = 3875
+dolares =  pesos/valor_dolar
+dolares = round(dolares,2)
+dolares = str(dolares) 
+print("Teines $" + dolares +" dolares")
+
