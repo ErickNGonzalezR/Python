c12426e92b805ac80e8b8a898cad8a34a80dcdfa
Erick Nicolas Gonzalez <nicolo288@hotmail.com>
2022-10-04 11:39:05 -0500

    arreglando errores

--- ejercicios_python/Conversor_Monedas.py
+++ ejercicios_python/Conversor_Monedas.py
@@ -7,24 +7,28 @@ Bienvenido a el conversor de moneda
 """
 opcion = input(menu)
 opcion = int(opcion)
-def conversor_moneda (valor_nomeda,pesos):
+
+def conversor_moneda (valor_dolar,pesos):
     dolares =  pesos/valor_dolar
     dolares = round(dolares,2)
     dolares = str(dolares) 
     print("Tienes $" + dolares +" dolares")
-if opcion ==1 :
-    conversor_moneda(3800,float("Cuantos pesos Colombianos tienes"))
-    
+    return dolares
 
+if opcion ==1 :
+    pesos = float(input ("Cuantos pesos Colombianos tienes "))
+    valor_dolar = 3800
+    conversor_moneda(valor_dolar,pesos)
+    
 elif opcion ==2:
-    pesos = float(input ("Cuantos pesos Argentinos tienes"))
+    pesos = float(input ("Cuantos pesos Argentinos tienes "))
     valor_dolar = 60
-    conversor_moneda()
+    conversor_moneda(valor_dolar,pesos)
 
 elif opcion ==3:
-    pesos = float(input ("Cuantos pesos Mexicanos tienes"))
+    pesos = float(input ("Cuantos pesos Mexicanos tienes "))
     valor_dolar = 24
-    conversor_moneda()
+    conversor_moneda(valor_dolar,pesos)
 
 else:
     print("porfavor elige una opcion correcta")    
